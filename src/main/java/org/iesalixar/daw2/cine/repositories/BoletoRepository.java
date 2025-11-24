ac525572686192a801cc811da2f55f02f1a0e711
Daniel <danielgongar8@gmail.com>
2025-11-24 15:23:17 +0100

    cambios en Boleto.java, BoletoRepository.java y pequeño cambio en Cliente.java

--- src/main/java/org/iesalixar/daw2/cine/repositories/BoletoRepository.java
+++ src/main/java/org/iesalixar/daw2/cine/repositories/BoletoRepository.java
@@ -1,11 +1,17 @@
 package org.iesalixar.daw2.cine.repositories;
 
 import org.iesalixar.daw2.cine.entities.Boleto;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface BoletoRepository {
-    Page<Boleto> findAll(Pageable pageable);
+public interface BoletoRepository extends JpaRepository<Boleto, Long> {
+    // Buscar por asiento
+    Page<Boleto> findByAsientoContainingIgnoreCase(String asiento, Pageable pageable);
 
-    Page<Boleto> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    // Buscar por cliente
+    Page<Boleto> findByClienteNombreContainingIgnoreCase(String nombre, Pageable pageable);
+
+    // Buscar por función
+    Page<Boleto> findByFuncionTituloContainingIgnoreCase(String titulo, Pageable pageable);
 }
