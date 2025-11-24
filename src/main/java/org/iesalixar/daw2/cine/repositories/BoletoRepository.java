9825be8d20f525ee552ee550a817aed58fb45901
Daniel <danielgongar8@gmail.com>
2025-11-24 14:42:49 +0100

    creacion repositorio Bolero

---
+++ src/main/java/org/iesalixar/daw2/cine/repositories/BoletoRepository.java
@@ -0,0 +1,11 @@
+package org.iesalixar.daw2.cine.repositories;
+
+import org.iesalixar.daw2.cine.entities.Boleto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface BoletoRepository {
+    Page<Boleto> findAll(Pageable pageable);
+
+    Page<Boleto> findByNameContainingIgnoreCase(String name, Pageable pageable);
+}
