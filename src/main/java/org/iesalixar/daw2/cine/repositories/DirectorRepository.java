d3692a25d8ace8049093797b0bb6db1a509ee061
jaimerm16 <jaimeramirezmuela@gmail.com>
2025-11-24 15:25:08 +0100

    Creación DirectorRepository.

--- src/main/java/org/iesalixar/daw2/cine/repositories/DirectorRepository.java
+++ src/main/java/org/iesalixar/daw2/cine/repositories/DirectorRepository.java
@@ -1,7 +1,23 @@
 package org.iesalixar.daw2.cine.repositories;
 
-import org.iesalixar.daw2.cine.entities.Director;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface DirectorRepository extends JpaRepository<Director, Long> {
+import java.util.List;
+import java.util.Optional;
 
+import org.iesalixar.daw2.cine.entities.Director;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface DirectorRepository extends JpaRepository<Director, Long> {
+    Page<Director> findAll(Pageable pageable);
+
+    Page<Director> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    long countByNameContainingIgnoreCase(String name);
+
+    boolean existsDirectorByCode(String code);
+    @Query("SELECT COUNT(p) > 0 FROM Director p WHERE p.code = :code AND p.id != :id")
+    boolean existsDirectorByCodeAndNotId(@Param("code") String code, @Param("id") Long id);
 }
